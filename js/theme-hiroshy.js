c50c46215382110c2067b9bcac867317b3fb323b
Hiroshy <hiroshywissell@gmail.com>
2020-07-23 18:57:10 -0500

    terminado

--- js/theme-hiroshy.js
+++ js/theme-hiroshy.js
@@ -1,4 +1,16 @@
-function carga(){
-	alert("página totalmente cargada")
+// https://web.whatsapp.com/send?phone=+5215535688499
+let wsp_btn=document.getElementById("whatsapp");
+
+function whatsappOk() {
+	if(screen.width<500){
+		wsp_btn.setAttribute('href','https://api.whatsapp.com/send?phone=5215620222283&text=¡Hola! Vi tu portafolio y me encantaria contactárte')
+	}else{
+		wsp_btn.setAttribute('href','https://web.whatsapp.com/send?phone=+5215620222283&amp;text=¡Hola! Vi tu portafolio y me encantaria contactárte')
+	}
 }
-window.addEventListener("load",carga)
+
+window.addEventListener("load",()=>{
+	whatsappOk()
+})
+
+window.addEventListener("resize",whatsappOk)
